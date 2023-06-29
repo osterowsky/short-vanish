fa9cc23884251b4324e848bf403daf531238bc47
Patryk Ostern <ostern.patryk02@gmail.com>
2023-06-29 22:05:45 +0200

    block reels button and make redirection

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+var currentURL = window.location.href;
+if (currentURL.includes("instagram.com")) {
+    disableReels();
+}
+
+// We disable whole reels on Instagram
+function disableReels() {
+    redirectURLS();
+    hideReelsButton();
+}
+
+
+
+// Section for blocking reels on Instagram
+function hideReelsButton() {
+    var reelsLogo = document.querySelector('a[href="/reels/"]');
+    // We get the 3rd parent node to not affect UI 
+    if (reelsLogo) {
+        var parentDiv = reelsLogo.closest('div:not([class])');
+        if (parentDiv) {
+            parentDiv.style.display = 'none';
+        }   
+    }
+}
+
+function redirectURLS() {
+    if (currentURL.includes("/reels/")) {
+        window.location.href = "https://www.instagram.com/";
+    }
+}
