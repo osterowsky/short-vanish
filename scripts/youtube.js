93d3b07a041a707db1b4e2c129f17b257abd9815
Patryk Ostern <ostern.patryk02@gmail.com>
2023-07-01 14:08:21 +0200

    unify youtube section

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -6,48 +6,40 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.message === 'youtube') {
         disableShorts(window.location.href);
     }
-  });
+});
 
 
 // We disable shorts on Youtube
 function disableShorts(url) {
     redirectURLs(url);
     hideButtons();
-
     // We check if we are in the results page or in the feed page to hide correct short sections
-    window.location.href.includes("youtube.com/results?") ? hideFromResults() : hideFromFeed();
-
-    // `MutationObserver` is used to detect when the unchanged shorts button is added to the DOM, if it wasn't detected before
-    startMutationObserver();
-
+    window.location.href.includes("youtube.com/results?") ? hideSections('ytd-reel-shelf-renderer') : hideSections('ytd-rich-shelf-renderer[is-shorts=""]');
 }
 
-// Section for Mutation Observer for Shorts Buttons
+// Section for Mutation Observer
 // ---------------------------------------
 var mutationObserver;
 
-function startMutationObserver() {
-
+function startMutationObserver(target) {
     var targetNode = document.querySelector('body');
     if (targetNode) {
         mutationObserver = new MutationObserver(function(mutationsList) {
             for (var mutation of mutationsList) {
                 if (mutation.type === 'childList') {
-                    var shortsButtons = document.querySelectorAll('a[title="Shorts"]');
-                    console.log(shortsButtons.length)
-                    if (shortsButtons.length > 1) {
+                    var shortsButtons = document.querySelectorAll('a[title="Shorts"]:not(.hidden-shorts-button)');
+                    if (shortsButtons.length > 0) {
                             shortsButtons.forEach(function (button) {
                                 hideButton(button);
-                            });
-                            stopMutationObserver();
-                            break;
-                    } else if (shortsButtons.length === 1) {
-                            hideButton(shortsButtons[0])
-                            if (window.location.href.includes("youtube.com/watch?")) {
-                                stopMutationObserver();
-                                break;
-                            }
-                    } 
+                        });
+                    }
+
+                    var shortsSections = document.querySelectorAll(target+':not(.hidden-shorts-section)');
+                    if (shortsSections.length > 0) {
+                        shortsSections.forEach(function (section) {
+                            hideSection(section);
+                        });
+                    }
                 }
             }
         });
@@ -55,7 +47,7 @@ function startMutationObserver() {
         var config = { childList: true, subtree: true };
         mutationObserver.observe(targetNode, config);
 
-        // Checks if we are in parts of youtube where there are no buttons
+        // Checks if we are in parts of youtube where there is no need to observe for shorts buttons
         if (window.location.href.includes("studio.youtube.com")) {
             stopMutationObserver();
         }
@@ -66,38 +58,14 @@ function stopMutationObserver() {
     mutationObserver.disconnect();
 }
 
-// Section for Feed Mutation Observer
-// ---------------------------------------
-
-var feedMutationObserver;
-
-function startFeedMutationObserver(target) {
-
-    var targetNode = document.querySelector('body');
-    if (targetNode) {
-        mutationObserver = new MutationObserver(function(mutationsList) {
-            for (var mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    var shortsSections = document.querySelectorAll(target+':not(.hidden-shorts-section)');
-                    if (shortsSections.length > 0) {
-                        shortsSections.forEach(function (section) {
-                            hideShortsSection(section);
-                        });
-                    }
-                }        
-            }
-        });
-
-        var config = { childList: true, subtree: true };
-        mutationObserver.observe(targetNode, config);
-}}
-
-function stopFeedMutationObserver() {
-    mutationObserver.disconnect();
-}
-
 // Section for blocking shorts on Youtube
 // ---------------------------------------
+ 
+function redirectURLs(url) {
+    if (url.includes("/shorts/")) {
+        window.location.href = "https://www.youtube.com/";
+    }
+}
 
 function hideButtons() {
     var shortsButtons = document.querySelectorAll('a[title="Shorts"]');
@@ -107,43 +75,24 @@ function hideButtons() {
            hideButton(button);
         });
     }
-    
 }
 
 function hideButton(button) {
     button.hidden = true;
     button.classList.add('hidden-shorts-button');
 }
- 
-function redirectURLs(url) {
-    if (url.includes("/shorts/")) {
-        window.location.href = "https://www.youtube.com/";
-    }
-}
 
-function hideFromFeed() {
-    var target = 'ytd-rich-shelf-renderer[is-shorts=""]';
+function hideSections(target) {
     var shortsSections = document.querySelectorAll(target);
     if (shortsSections.length > 0) {
         shortsSections.forEach(function (section) {
-            hideShortsSection(section);
+            hideSection(section);
         });
     }
-    startFeedMutationObserver(target);
+    startMutationObserver(target);
 }
 
-function hideFromResults() {
-    var target = 'ytd-reel-shelf-renderer';
-    var shortsSections = document.querySelectorAll(target);
-    if (shortsSections.length > 0) {
-        shortsSections.forEach(function (section) {
-           hideShortsSection(section);
-        });
-    }
-    startFeedMutationObserver(target);
-}
-
-function hideShortsSection(section) {
+function hideSection(section) {
     section.hidden = true;
     section.classList.add('hidden-shorts-section');
 }
