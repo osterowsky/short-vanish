4b1a83510a419fa498a08e063ae96e0ccceac28f
Patryk Ostern <ostern.patryk02@gmail.com>
2023-07-06 11:23:27 +0200

    improve the individual buttons

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,26 @@
 const enablePlugin = document.getElementById("enablePlugin");
+const instagramPlugin = document.getElementById("enableInstagram");
+const youtubePlugin = document.getElementById("enableYoutube");
 
   enablePlugin.addEventListener("change", function() {
     // Store the checkbox state in the Storage API
-    chrome.storage.local.set({ 'enablePlugin':  enablePlugin.checked});
-  });
+    const isChecked = enablePlugin.checked;
+    instagramPlugin.checked = isChecked;
+    youtubePlugin.checked = isChecked;
+  
+    // Store the checkbox states in the Storage API
+    chrome.storage.local.set({
+      'enablePlugin': isChecked,
+      'instagramPlugin': isChecked,
+      'youtubePlugin': isChecked
+    });  
+});
 
   document.addEventListener("DOMContentLoaded", function() {
     // Get the checkbox state from the Storage API and set the initial checkbox state
-    chrome.storage.local.get('enablePlugin', function(result) {
+    chrome.storage.local.get(['enablePlugin', 'instagramPlugin', 'youtubePlugin'], function(result) {
       enablePlugin.checked = result.enablePlugin || false;
+      instagramPlugin.checked = result.instagramPlugin || false;
+      youtubePlugin.checked = result.youtubePlugin || false;
     });
   });
